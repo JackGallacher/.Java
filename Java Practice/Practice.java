801dc6ba2f6b93ea904bb9332e74430a89a0c3bc
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-06-24 23:25:39 +0100

    Inheritance, Polymorphism and Abstract
    - Added an example that used an abstract class with abstract functions,
    overloaded those in an inherited class, and used polymorphism to access
    a function within the abstract class.

--- Java Practice/Practice.java
+++ Java Practice/Practice.java
@@ -2,9 +2,17 @@ class Program
 {
 	public static void main(String[]args)
 	{
-		Calculator MyCalculator = new Calculator();
+		//Instantiation and usage of the Calculator class.
+		//Calculator MyCalculator = new Calculator();		
+		//MyCalculator.Square(5);
 		
-		MyCalculator.Square(5);
+		//Usage of the Animal class through polymorphism.
+		Animal MyAnimal = new Lion();
+		MyAnimal.Sound();
+		MyAnimal.Diet();
+		
+		MyAnimal.PrintFromAbstract();//We can acces the abstract class function through the child class.
+		
 	}
 }
 class Calculator
@@ -31,3 +39,31 @@ class Calculator
 		System.out.format("The square of %s is %s", x, Result);
 	}
 }
+
+abstract class Animal
+{
+	abstract void Sound();//We declare these as abstract becuase we want the actual implementation of these to be declared in thier actual child classes. These MUST be overridden.
+	abstract void Diet();	
+	
+	void PrintFromAbstract()
+	{
+		System.out.println("This is being printed from the abstract class Animal, to get to this we had to instantiate the type of this class through polymorhism of its child class. e.g. Animal x = new Lion();");
+	}
+	
+}
+class Lion extends Animal
+{
+	void Sound()
+	{
+		System.out.println("ROAR!\n");
+	}
+	void Diet()
+	{
+		System.out.println("Meat.\n");
+	}
+	
+}
+
+//Interfaces example
+//Method overloading example.
+//Input/Output example
