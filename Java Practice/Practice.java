58a4aa847e9b93b5f936a7f7256edb0f846f60c4
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-06-26 23:42:10 +0100

    Added loop example
    - Used a for loop, its the same as C#!

--- Java Practice/Practice.java
+++ Java Practice/Practice.java
@@ -1,20 +1,57 @@
+import java.util.Scanner;
+
 class Program
 {
 	public static void main(String[]args)
 	{
 		//Instantiation and usage of the Calculator class.
-		//Calculator MyCalculator = new Calculator();		
+		Calculator MyCalculator = new Calculator();		
 		//MyCalculator.Square(5);
 		
 		//Usage of the Animal class through polymorphism.
-		Animal MyAnimal = new Lion();
-		MyAnimal.Sound();
-		MyAnimal.Diet();
+		//Animal MyAnimal = new Lion();
+		//MyAnimal.Sound();
+		//MyAnimal.Diet();		
+		//MyAnimal.PrintFromAbstract();//We can acces the abstract class function through the child class.
 		
-		MyAnimal.PrintFromAbstract();//We can acces the abstract class function through the child class.
+		//Usage of an interface from a class that "implements it".
+		//MediaControls Media = new MediaControls();
+		//Media.Play();
+		//Media.Pause();
+		//Media.Record();
+		//Media.Stop();
 		
+		//User input example.
+		String MyString;		
+		System.out.println("Enter something into the console...");
+		
+		Scanner Scan = new Scanner(System.in);
+		MyString = Scan.nextLine();
+	
+		System.out.format("You just entered %s\n", MyString);
+		
+		int FirstInt = 0;
+		int SecondInt = 0;
+		System.out.println("Enter first number");
+		FirstInt = Scan.nextInt();
+		
+		System.out.println("Enter second number");
+		SecondInt = Scan.nextInt();
+		
+		System.out.format("%s + %s = %s", FirstInt, SecondInt, MyCalculator.Add(FirstInt, SecondInt));
+		Scan.close();
+		
+		System.out.println("\n" + Integer.toString(FirstInt) + " " + Integer.toString(SecondInt));//Converts int to string and concatinates varaibles into strings.
+		
+		//Loop example.
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.print(i + " ");
+		}		
 	}
 }
+
+//Class example
 class Calculator
 {
 	int Add(int x, int y)
@@ -40,6 +77,7 @@ class Calculator
 	}
 }
 
+//Abstract class and method overloading example.
 abstract class Animal
 {
 	abstract void Sound();//We declare these as abstract becuase we want the actual implementation of these to be declared in thier actual child classes. These MUST be overridden.
@@ -65,5 +103,67 @@ class Lion extends Animal
 }
 
 //Interfaces example
+interface Remote
+{
+	public void Play();
+	public void Pause();
+	public void Record();
+	public void Stop();
+}
+class MediaControls implements Remote
+{
+	public void Play()//Implementations of interfaces need to be public.
+	{
+		System.out.println("Play has been pressed.\n");
+	}
+	public void Pause()
+	{
+		System.out.println("Pause has been pressed.\n");
+	}
+	public void Record()
+	{
+		System.out.println("Record has been pressed.\n");
+	}
+	public void Stop()
+	{
+		System.out.println("Stop has been pressed.\n");
+	}
+}
+
+
+
 //Method overloading example.
 //Input/Output example
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
