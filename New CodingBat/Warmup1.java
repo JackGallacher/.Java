d125d5702e4f0a4e1796e55b61a9fa7c09dcca98
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-08-09 21:37:17 +0100

    More codingbat solutions
    - More solutions from the "Warmup" section of CodingBat

--- New CodingBat/Warmup1.java
+++ New CodingBat/Warmup1.java
@@ -13,6 +13,36 @@ class Warmup1
 		Tasks.MonkeyTrouble(true, true);//true
 		Tasks.MonkeyTrouble(false, false);//true
 		Tasks.MonkeyTrouble(true, false);//false
+		
+		//3		
+		System.out.println(Integer.toString(Tasks.SumDouble(1,2)));//3
+		System.out.println(Integer.toString(Tasks.SumDouble(3,2)));//5
+		System.out.println(Integer.toString(Tasks.SumDouble(2,2)));//8
+		
+		//4
+		System.out.println(Integer.toString(Tasks.Diff21(19)));//2
+		System.out.println(Integer.toString(Tasks.Diff21(10)));//11
+		System.out.println(Integer.toString(Tasks.Diff21(21)));//0
+		
+		//5
+		Tasks.ParrotTrouble(true, 6);//true
+		Tasks.ParrotTrouble(true, 7);//false
+		Tasks.ParrotTrouble(false, 6);//false
+		
+		//6
+		Tasks.Makes10(9, 10);//true
+		Tasks.Makes10(9, 9);//false
+		Tasks.Makes10(1, 9);//true
+		
+		//7
+		Tasks.NearHundred(93);//true
+		Tasks.NearHundred(90);//true
+		Tasks.NearHundred(89);//false
+		
+		//8
+		Tasks.PosNeg(1, -1, false);//true
+		Tasks.PosNeg(-1, 1, false);//true
+		Tasks.PosNeg(-4, -5, true);//true
 	}
 	boolean SleepIn(boolean Weekday, boolean Vacation)
 	{
@@ -34,5 +64,129 @@ class Warmup1
 		System.out.println("Phew, We are not in trouble.");
 		return false;
 	}
+	int SumDouble(int A, int B)
+	{
+		if(A == B)
+		{
+			return (A + B) * 2;
+		}
+		return A + B;
+		
+	}
+	int Diff21(int N)
+	{
+		if(N > 21)
+		{
+			return Math.abs((21 - N) * 2);//Returns absolute value (Always positive.)
+		}
+		return Math.abs(N - 21);		
+	}
+	boolean ParrotTrouble(boolean Talking, int Hour)
+	{
+		if(Talking)
+		{
+			if(Hour < 7 || Hour > 20)
+			{
+				System.out.println("We are in trouble!");
+				return true;
+			}
+		}
+		System.out.println("We are not in trouble!");
+		return false;		
+	}
+	boolean Makes10(int A, int B)
+	{
+		if(A == 10 || B == 10 || A + B == 10)
+		{
+			System.out.println("One of the numbers is 10 or the sum is 10.");
+			return true;
+		}
+		return false;
+	}
+	boolean NearHundred(int N)
+	{
+		if(Math.abs(100 - N) <= 10 || Math.abs(200 - N) <= 10 )
+		{
+			System.out.println("Withing 10 if 100 or 200.");
+			return true;
+		}
+		return false;
+	}
+	boolean PosNeg(int A, int B, boolean Negative)
+	{
+		if(Negative)
+		{
+			if(A < 0 && B < 0)
+			{
+				System.out.println("Both values are negative.");
+				return true;
+			}
+			return false;
+		}
+		if(A < 0 && B > 0 || B < 0 && A > 0)
+		{
+			System.out.println("One value is negative and one is positive.");
+			return true;
+		}
+		return false;
+	}
+	String NotString(String Str)
+	{
+		if(Str.equals("not " + Str))
+		{
+			return Str;
+		}
+		else
+		{
+			return "not " + Str;
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 }
