0510699d135422604de0c0c402e54d2124f6974f
Jack Gallacher <jackleslie.gallacher@gmail.com>
2016-12-21 07:40:24 +0000

    Testing Protected members
    - Quick test for reference on how protected members can be accesed by
    subclasses in another package through inheritance.

---
+++ Java OCA Exercises/Access Control/src/package2/Program.java
@@ -0,0 +1,8 @@
+package package2;
+public class Program {
+
+	public static void main(String[] args) {
+		Dog myDog = new Dog();
+		myDog.dogTalk();
+	}
+}
--- Java OCA Exercises/Apple.java
+++
@@ -1,6 +0,0 @@
-import food.Fruit;
-class Apple extends Fruit{
-	public Apple(String type){
-		super(type);
-	}
-}
