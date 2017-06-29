f7fa4f988578f6714ef062061d01dbd31a0643f8
Jack Gallacher <jackleslie.gallacher@gmail.com>
2017-06-29 17:18:34 +0100

    Added book entity, language enum and persistance
    Added book entity, language enum and persistance

---
+++ Bookstore/src/main/java/com/bookstore/model/Book.java
@@ -0,0 +1,120 @@
+package com.bookstore.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Jack on 29/06/2017.
+ */
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 200)
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column(name = "unit_cost")
+    private Float unitCost;
+
+    private String isbn;
+
+    @Column(name = "publication_date")
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+    private int numberOfPages;
+    private String imageURL;
+    private Language language;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", isbn='" + isbn + '\'' +
+                ", publicationDate=" + publicationDate +
+                ", numberOfPages=" + numberOfPages +
+                ", imageURL='" + imageURL + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
---
+++ Bookstore/src/main/java/com/bookstore/model/Language.java
@@ -0,0 +1,8 @@
+package com.bookstore.model;
+
+/**
+ * Created by Jack on 29/06/2017.
+ */
+public enum Language {
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN, FINNISH, GERMAN, DEUTSCH, RUSSIAN;
+}
